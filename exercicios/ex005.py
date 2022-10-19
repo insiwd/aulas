cd014f4b80ccda46b7d718f5566ff917aaba330a
rwan <ruan.cavalcante505@gmail.com>
2022-10-19 15:54:29 -0300

    melhorias

---
+++ exercicios/ex005.py
@@ -0,0 +1,8 @@
+#programa que diz o antecessor e o sucessor do número escrito
+
+n = int(input('digite um número: '))
+
+antecessor = n - 1
+sucessor = n + 1
+
+print(f'analisando {n}, seu antecessor é {antecessor} e seu sucessor é {sucessor}')
