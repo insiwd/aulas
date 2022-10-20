5b8012fbfe77224b79c15efc523a9303f6a2565c
rwan <ruan.cavalcante505@gmail.com>
2022-10-20 14:50:35 -0300

    .

---
+++ exercicios/ex009.py
@@ -0,0 +1,28 @@
+# tabuada
+
+nmr = int(input('digite um número: '))
+
+print('-' * 12)
+x1 = nmr  
+x2 = nmr + nmr
+x3 = nmr + x2
+x4 = nmr + x3
+x5 = nmr + x4
+x6 = nmr + x5
+x7 = nmr + x6
+x8 = nmr + x7
+x9 = nmr + x8
+x10 = nmr + x9
+
+print(f'{nmr} x 1 = {x1}')
+print(f'{nmr} x 2 = {x2}')
+print(f'{nmr} x 3 = {x3}')
+print(f'{nmr} x 4 = {x4}')
+print(f'{nmr} x 5 = {x5}')
+print(f'{nmr} x 6 = {x6}')
+print(f'{nmr} x 7 = {x7}')
+print(f'{nmr} x 8 = {x8}')
+print(f'{nmr} x 9 = {x9}')
+print(f'{nmr} x 10 = {x10}')
+
+print('-' * 12)
